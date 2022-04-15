987eaa623650eed31aa6075f59138ab8a13b923f
seonggon do <62453668+mooh2jj@users.noreply.github.com>
2022-04-15 21:54:16 +0900

    dependency setting

---
+++ src/main/java/com/dsg/junit_example/JunitExampleApplication.java
@@ -0,0 +1,13 @@
+package com.dsg.junit_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunitExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JunitExampleApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/dsg/junit_example/model/Employee.java
@@ -0,0 +1,30 @@
+package com.dsg.junit_example.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "employees")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "latt_name", nullable = false)
+    private String lastName;
+
+    @Column(nullable = false)
+    private String email;
+}
