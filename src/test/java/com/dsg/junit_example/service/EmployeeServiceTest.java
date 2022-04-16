4a62f8828ba6f0f4912b6bcf096b1416950c7eec
seonggon do <62453668+mooh2jj@users.noreply.github.com>
2022-04-17 02:42:53 +0900

    Unit test for saveEmployee method which throws Exception

--- src/test/java/com/dsg/junit_example/service/EmployeeServiceTest.java
+++ src/test/java/com/dsg/junit_example/service/EmployeeServiceTest.java
@@ -1,9 +1,11 @@
 package com.dsg.junit_example.service;
 
+import com.dsg.junit_example.exception.ResourceNotFoundException;
 import com.dsg.junit_example.model.Employee;
 import com.dsg.junit_example.repository.EmployeeRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -12,7 +14,11 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
 
 @Slf4j
 @ExtendWith(MockitoExtension.class)
@@ -24,18 +30,24 @@ class EmployeeServiceTest {
     @InjectMocks
     private EmployeeServiceImpl employeeService;
 
-    @Test
-    public void given_when_then(){
-        // given - precondition or setup
-        Employee employee = Employee.builder()
+    private Employee employee;
+
+    @BeforeEach
+    void setup() {
+        employee = Employee.builder()
                 .id(1L)
                 .firstName("dsg")
                 .lastName("ddd")
                 .email("ehtjd333@gmail.com")
                 .build();
+    }
+
+    @Test
+    public void given_when_then(){
+        // given - precondition or setup
+
 //        given(employeeRepository.findByEmail(employee.getEmail()))
 //                .willReturn(Optional.empty());
-
         given(employeeRepository.save(employee))
                 .willReturn(employee);
 
@@ -47,7 +59,32 @@ class EmployeeServiceTest {
         log.info("savedEmployee: {}", savedEmployee);
 
         // then - verify the output
-        Assertions.assertThat(savedEmployee).isNotNull();
+        assertThat(savedEmployee).isNotNull();
+
+    }
+
+    @Test
+    public void given_when_thenThrowsException(){
+        // given - precondition or setup
+        given(employeeRepository.findByEmail(employee.getEmail()))
+                .willReturn(Optional.of(employee));
+
+//        given(employeeRepository.save(employee))
+//                .willReturn(employee);
+
+        log.info("employeeRepository: {}", employeeRepository);
+        log.info("employeeService: {}", employeeService);
+
+        // when - action or the behaviour that we are going test
+//        org.junit.jupiter.api.Assertions.assertThrows(ResourceNotFoundException.class, ()->{
+//            employeeService.saveEmployee(employee);
+//        });
+        assertThatThrownBy(() -> {
+            employeeService.saveEmployee(employee);
+        }).isInstanceOf(ResourceNotFoundException.class);
+
+        // then - verify the output
+        verify(employeeRepository, never()).save(any(Employee.class));
 
     }
 
